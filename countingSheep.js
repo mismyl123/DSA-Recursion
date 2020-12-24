ae62abfa66b94277416784240d5ef2f9b8599a29
mismyl123 <mlalonde309@gmail.com>
2020-12-23 19:43:01 -0500

    separated drills

---
+++ countingSheep.js
@@ -0,0 +1,15 @@
+
+//Counting Sheep
+
+function countingSheep(sheeps) {
+    //base case
+    if (sheeps === 0) return "All sheep jumped over the fence";
+    //general case
+    return (
+      `${sheeps}: Another sheep jumps over the fence\n` +
+      countingSheep(sheeps - 1)
+    );
+  }
+  
+  //calling with input = 3
+  console.log(countingSheep(3));
